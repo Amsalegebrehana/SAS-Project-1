a2118a9bebf51446ba59d6e7cc467f9ad779d114
Selamawit926 <se.selamawit.elias@gmail.com>
2021-02-14 15:15:52 +0300

    Carousel is working in each_service page for people's profile

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,38 +1,3 @@
-const con=document.getElementById("con");
-const service1=document.getElementById("service");
-const service2=document.getElementById("service2");
-const cont=document.getElementById("cont");
-function clicked(){
-    
-    
-    if(con.style.marginTop!="300px"){
-        con.style.marginTop="300px";
-        service1.style.display="none";
-        service2.style.display="block";
-    }
-    else{
-        con.style.marginTop="0px";
-    }
-
-    
-}
-
-
-function clicked2(){
-    
-    
-    if(cont.style.marginTop!="300px"){
-        cont.style.marginTop="300px";
-        service1.style.display="none";
-        service2.style.display="block";
-    }
-    else{
-        cont.style.marginTop="0px";
-    }
-
-    
-}
-
 /*
     Carousel
 */
