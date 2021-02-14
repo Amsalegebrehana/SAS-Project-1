00b2b554438779c093c25e7c1e380591aa28fe40
Selamawit926 <se.selamawit.elias@gmail.com>
2021-02-14 13:29:05 +0300

    Navbar updated to be responsive in each page using js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,30 @@
-const con=document.getElementById("con");
+const firstPic= document.getElementById("picc");
 const service1=document.getElementById("service");
 const service2=document.getElementById("service2");
-const cont=document.getElementById("cont");
+const about=document.getElementById("about");
 function clicked(){
     
     
-    if(con.style.marginTop!="300px"){
-        con.style.marginTop="300px";
+    if(firstPic.style.marginTop!="300px"){
+        firstPic.style.marginTop="300px";
         service1.style.display="none";
         service2.style.display="block";
     }
     else{
-        con.style.marginTop="0px";
+        firstPic.style.marginTop="0px";
     }
 
     
 }
-
 
 function clicked2(){
-    
-    
-    if(cont.style.marginTop!="300px"){
-        cont.style.marginTop="300px";
+    if(about.style.marginTop!="300px"){
+        about.style.marginTop="300px";
         service1.style.display="none";
         service2.style.display="block";
     }
     else{
-        cont.style.marginTop="0px";
+        about.style.marginTop="0px"; 
     }
-
-    
 }
+
