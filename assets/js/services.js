a87430a0ed48cc5be46e494ac3b3432f56190c1a
Selamawit926 <se.selamawit.elias@gmail.com>
2021-02-20 12:11:57 +0300

    Nav bar is updated to include popular services

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,14 +1,14 @@
 const con=document.getElementById("con");
 const service1=document.getElementById("service");
-const service2=document.getElementById("service2");
+// const service2=document.getElementById("service2");
 const cont=document.getElementById("cont");
 function clicked(){
     
     
-    if(con.style.marginTop!="300px"){
-        con.style.marginTop="300px";
-        service1.style.display="none";
-        service2.style.display="block";
+    if(con.style.marginTop!="400px" ){
+        con.style.marginTop="400px";
+        // service1.style.display="none";
+        // service2.style.display="block";
     }
     else{
         con.style.marginTop="0px";
@@ -17,14 +17,13 @@ function clicked(){
     
 }
 
-
 function clicked2(){
     
     
-    if(cont.style.marginTop!="300px"){
-        cont.style.marginTop="300px";
-        service1.style.display="none";
-        service2.style.display="block";
+    if(cont.style.marginTop!="400px"){
+        cont.style.marginTop="400px";
+        // service1.style.display="none";
+        // service2.style.display="block";
     }
     else{
         cont.style.marginTop="0px";
