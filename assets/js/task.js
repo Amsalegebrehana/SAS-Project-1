9cfacd12399099126369ab2c35ff6e0b46531e3c
Amsalegebrehana <57097500+Amsalegebrehana@users.noreply.github.com>
2021-02-21 10:10:03 +0300

    Third b (#10)
    * Confirmation page completed
    
    * Searching functionality
    
    * Styling the added pages

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,14 +1,26 @@
 const firstPic= document.getElementById("picc");
-const service1=document.getElementById("service");
-const service2=document.getElementById("service2");
+// const service1=document.getElementById("service");
+const expect= document.getElementById("epect");
+const seLink=document.getElementById("seelink");
+// const service2=document.getElementById("service2");
 const about=document.getElementById("about");
+
+
+function detail(){
+    if(expect.style.display == 'none'){
+        expect.style.display = 'block';
+    }
+    else{
+        expect.style.display = 'none';
+    }
+}
 function clicked(){
     
     
     if(firstPic.style.marginTop!="300px"){
         firstPic.style.marginTop="300px";
-        service1.style.display="none";
-        service2.style.display="block";
+        // service1.style.display="none";
+        // service2.style.display="block";
     }
     else{
         firstPic.style.marginTop="0px";
@@ -20,40 +32,11 @@ function clicked(){
 function clicked2(){
     if(about.style.marginTop!="300px"){
         about.style.marginTop="300px";
-        service1.style.display="none";
-        service2.style.display="block";
+        // service1.style.display="none";
+        // service2.style.display="block";
     }
     else{
         about.style.marginTop="0px"; 
     }
-}
-document.addEventListener("DOMContentLoaded", function(event) {
+};
 
-    var div = document.getElementById('mode');
-    div.addEventListener('click', function(e){
-    
-    var li = document.getElementById('card-mode');
-    var bgdark = document.getElementById('dark-background');
-    
-    if (li.className.split(' ')[1] == "card-dark") {
-    
-    li.classList.remove('card-dark');
-    bgdark.classList.remove('bg-dark');
-    
-    
-    }else{
-    
-    
-    
-    
-    li.classList.add('card-dark');
-    bgdark.classList.add('bg-dark');
-    
-    }
-    
-    
-    });
-    
-    
-    
-    });
