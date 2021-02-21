8b5bed9af73804c895a4c3ef0dbbedd75ad33deb
sitota49 <se.sitota.ezra@gmail.com>
2021-02-21 06:24:48 +0300

    Acc.html modified

--- assets/js/acc.js
+++ assets/js/acc.js
@@ -3,17 +3,19 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = Number(urlParams.get('id'));
 const logOutBtn = document.querySelector('#logOutBtn');
 const profileSec = document.querySelector('.addOn');
+const navItems = document.querySelector('.myNavItems');
+
 
 function updateUI(user) {
-    if (user.isLoggedIn) {
-        myP.innerHTML = "Logged In"
-    } else {
-        myP.innerHTML = "Not"
-    }
+  if (user.isLoggedIn) {
+    myP.innerHTML = "Logged In"
+  } else {
+    myP.innerHTML = "Not"
+  }
 
 
 
-    profileSec.innerHTML = ` 
+  profileSec.innerHTML = ` 
     <div class="row>
    
     <div class="col-4">
@@ -35,7 +37,7 @@ function updateUI(user) {
 };
 
 getUserInfo({ id }).then(function (user) {
-    updateUI(user);
+  updateUI(user);
 });
 
 
@@ -43,13 +45,13 @@ logOutBtn.addEventListener('click', logOut);
 
 function logOut() {
 
-    getUserInfo({ id }).then(function (user) {
+  getUserInfo({ id }).then(function (user) {
 
-        user.isLoggedIn = false;
-        update(id, user).then(function () {
-            window.location.href = `login.html`;
-            updateUI(user);
-        })
-    });
+    user.isLoggedIn = false;
+    update(id, user).then(function () {
+      window.location.href = `login.html`;
+      updateUI(user);
+    })
+  });
 
 }
